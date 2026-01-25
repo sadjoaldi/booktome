5bf0b2b1adf8d1deed5bbecb46cec6b51b91155d
Sadjoaldi <alsannealdi@hotmail.com>
2026-01-25 06:34:24 +0100

    fxt: add new loader

--- frontend/src/components/loader.tsx
+++ frontend/src/components/loader.tsx
@@ -3,9 +3,15 @@ import "../styles/loader.css";
 export default function Loader() {
   return (
     <div className="loader">
-      <div className="load-content">
-        <div />
-        <p>Chargement en cours...</p>
+      <div className="dot-spinner">
+        <div className="dot-spinner__dot" />
+        <div className="dot-spinner__dot" />
+        <div className="dot-spinner__dot" />
+        <div className="dot-spinner__dot" />
+        <div className="dot-spinner__dot" />
+        <div className="dot-spinner__dot" />
+        <div className="dot-spinner__dot" />
+        <div className="dot-spinner__dot" />
       </div>
     </div>
   );
